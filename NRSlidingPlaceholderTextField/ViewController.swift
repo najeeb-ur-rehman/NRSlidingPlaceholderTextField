16150cb80f09a8c9c20cd3d75862dcba0a182bd0
najeeb_ur_rehman <najeeburrehmanf13@gmail.com>
2020-04-25 17:02:33 +0500

    Initial Commit

---
+++ NRSlidingPlaceholderTextField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NRSlidingPlaceholderTextField
+//
+//  Created by Mian Najeeb on 4/25/20.
+//  Copyright © 2020 Mian Najeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
